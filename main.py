d31f8db536bd24b0e034f2566b7813e6ff480734
nour.s <nour.s@artec-int.com>
2022-02-04 15:54:06 +0000

    [Updates] remove resource copy class add process class implement some functionalities

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import resource
-
-
 def type_input(type_to_input, *input_message):
     value = input(*input_message)
     while type(value) != type_to_input:
@@ -12,32 +9,109 @@ def type_input(type_to_input, *input_message):
     else:
         return value
 
-class ResourceCopy:
-    def __init__(self, **kwargs) -> None:
+class Resource:
+    def __init__(self, **kwargs):
         self.possible_types = ('Hardware', 'Software')
         self.type = kwargs.get('type', self.possible_types[0])
         self.possible_states = ('Free', 'Busy')
         self.state = kwargs.get('state', self.possible_states[0])
 
-    def _switch_state(self) -> None:
-        self.state = self.possible_states[self.state == self.possible_states[0]]
+    def _is_free(self) -> bool:
+        return self.state == 'Free'
 
-class Resource:
-    def __init__(self, **kwargs) -> None:
-        self.possible_types = ('Hardware', 'Software')
-        self.type = kwargs.get('type', self.possible_types[0])
-        self.copy_number = kwargs.get('copy_namber', 1)
-        self.possible_states = ('Free', 'Busy')
-        self.free_copies = kwargs.get('free_copies', self.copy_number)
-        if self.free_copies > self.copy_number:
-            raise ValueError('The number of free copies should be lower than or equal the total number of copies')
-        self.busy_copies = kwargs.get('busy_copies', self._compute_busy_copies())
-        if self.busy_copies + self.free_copies != self.copy_number:
-            raise ValueError('The sum of the busy copies and the free copies should be equal to the total number of copies')
-        self.state = kwargs.get('state', 'Free')
+    def _reserve(self):
+        self.state = 'Busy'
 
-    def _compute_busy_copies(self) -> int:
-        return self.copy_number - self.free_copies
+    def _free(self):
+        self.state = 'Free'
+
+class Process:
+    process = []
+    process_index = 0
+    waiting_line = []
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', f'T{self.__class__.process_index + 1}')
+        self.priority = kwargs.get('priority', 0)
+        self.possible_states = ('New', 'Active', 'Blocked', 'Finished')
+        self.state = kwargs.get('state', self.possible_states[0])
+        self.arrival_time = kwargs.get('arrival_time', 0)
+        self.__class__.process.append(self)
+        self.__class__.process_index += 1
+        self.__class__._update_waiting_line(self)
+
+    def __repr__(self) -> str:
+        return '{name: %s, priority: %d, state: %s, arrival time: %d}' % (self.name, self.priority, self.state, self.arrival_time)
+
+    def _is_waiting(self) -> bool:
+        return self.state in ('New', 'Blocked')
+
+    def _check_waiting(self):
+        if not self._is_waiting(): 
+            print(f'The {self.state} resource can not reserve a resource')
+            raise
+
+    def _execute(self):
+        self.state = 'Active'
+
+    @classmethod
+    def _get_active_process(cls):
+        for process in cls.process:
+            if process._is_active():
+                return process
+
+    @classmethod
+    def _inhertit_priority(cls, active_process, blocked_process):
+        active_process.inherited_priority = blocked_process.inherited_priority
+
+    @classmethod
+    def _update_waiting_line(cls, process):
+        waiting_line = cls.waiting_line
+        if not process in waiting_line:
+            waiting_line.append(process)
+        cls.waiting_line = sorted(waiting_line, key=lambda p: (p.priority, p.arrival_time))
+
+
+    @classmethod
+    def _get_highets_waiting_priority_process(cls):
+        return cls.waiting_line[0]
     
-    def compute_busy_copies(self) -> None:
-        self.busy_copies = self._compute_busy_copies()      
+    @classmethod 
+    def inherit_priority(cls):
+        active_process = cls._get_active_process()
+        blocked_process = cls._get_highets_waiting_priority_process()
+        cls._inhertit_priority(active_process, blocked_process)
+
+    def _reserve_resource(self, resource: Resource):
+        self._check_waiting()
+        if resource._is_free():
+            resource._reserve()
+            self._execute()
+        else: 
+            self.__class__._get_active_process()
+
+    def _is_active(self) -> bool:
+        return self.state == 'Active'
+
+    def _check_active(self):
+        if not self._is_active():
+            print(f'The {self.state} resource can not free a resource')
+            raise
+        
+
+    def _free_resource(self, resource: Resource):
+        self._check_active()
+        if resource._is_free(): 
+            print('The resource is already free')
+            raise
+        resource._free()
+        self.__class__._get_highets_waiting_priority_process()._reserve_resource(resource)
+
+        
+
+
+p1 = Process(priority=5)
+p2 = Process(priority=3, arrival_time=3)
+p3 = Process(priority=3, arrival_time=4)
+
+# print(len(list(Process._get_process())))
+print(Process._get_highets_waiting_priority_process())
